3aca2478671f1474dc2f43a27d774ecaa0f5a553
Theofilus Handoyo <realth99@gmail.com>
2026-01-31 21:21:14 +0700

    feat: add open-api scalar

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
-import { Hono } from "hono";
+// import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { logger } from "hono/logger";
 import { commonRoute } from "./modules/common/route";
 import { characterRoute } from "./modules/characters/route";
+import { Scalar } from "@scalar/hono-api-reference";
+import { swaggerUI } from "@hono/swagger-ui";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
 app.use(logger());
-app.route("/", commonRoute);
+app.route("/commont-route", commonRoute);
 app.route("/characters", characterRoute);
+
+app.doc("/openapi.json", {
+  openapi: "3.0.4",
+  info: {
+    version: "1.0.0",
+    title: "Harry Potter API",
+    description: "A RESTful API providing structured data from the Harry Potter universe, including characters, Hogwarts houses, spells, potions, and magical artifacts.",
+    contact: {
+      name: "API Support Team",
+      url: "https://github.com/Theofilush/harry-potter-api",
+      email: "support@harrypotterapi.example.com",
+    },
+    license: {
+      name: "MIT License",
+      url: "https://opensource.org/licenses/MIT",
+    },
+  },
+  servers: [
+    {
+      url: "https://harry-potter-api-production-53c6.up.railway.app/",
+      description: "Production server",
+    },
+    {
+      url: "http://localhost:3000",
+      description: "Local development server",
+    },
+  ],
+});
+
+app.get("/", Scalar({ url: "/openapi.json" }));
+app.get("/swagger-ui", swaggerUI({ url: "/openapi.json" }));
 
 export default app;
--- src/modules/characters/schema.ts
+++ src/modules/characters/schema.ts
@@ -55,19 +55,17 @@ export const ParamsSchema = z.object({
     }),
 });
 
-export const UserSchema = z
-  .object({
-    id: z.string().openapi({
-      example: "123",
-    }),
-    name: z.string().openapi({
-      example: "John Doe",
-    }),
-    age: z.number().openapi({
-      example: 42,
-    }),
-  })
-  .openapi("User");
+export const UserSchema = z.object({
+  id: z.string().openapi({
+    example: "123",
+  }),
+  name: z.string().openapi({
+    example: "John Doe",
+  }),
+  age: z.number().openapi({
+    example: 42,
+  }),
+});
 
 export const CharactersSchema = z.array(CharacterSchema);
 
